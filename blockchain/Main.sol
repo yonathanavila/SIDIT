3db5f8d98585c3d7c117b122ffb9f49b76c15a6c
Yonathan Cruz <yonathancruz2019@gmail.com>
2022-11-26 15:15:45 -0600

    Fix roles

--- blockchain/Main.sol
+++ blockchain/Main.sol
@@ -2,37 +2,42 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./TypesLibrarie/Types.sol";
+import "./TypeLibraries/Types.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Main is Ownable {
-
     bool public StatusPaused;
 
     /// @dev Mapping to identify the sender role
     mapping(address => bool) isFireman;
 
     /// @dev Mapping to indentify the report
-    mapping(uint => Types.SIDIType) IdentifyReport;
+    mapping(uint256 => Types.SIDIType) IdentifyReport;
 
     /// @dev Modifier to identify the status pausable of the contract
-    modifier Status(){
-        require(StatusPaused == true, "The contract is paused you can't call this function");
+    modifier Status() {
+        require(
+            StatusPaused == true,
+            "The contract is paused you can't call this function"
+        );
         _;
     }
 
     /// @dev Modifier to validate the user rol
-    modifier onlyFiremen (){
-        require(isFireman[msg.sender] == false, "You don't have access to this function");
+    modifier onlyFiremen() {
+        require(
+            isFireman[msg.sender] == false,
+            "You don't have access to this function"
+        );
         _;
     }
 
-    constructor(){
+    constructor() {
         StatusPaused = false;
     }
 
-    /** 
-     * @dev Function to register a new Report 
+    /**
+     * @dev Function to register a new Report
      * @param _Name This is the name of the person that report the incident
      * @param _PhoneNumber This is the phone number of the person that call to 611 or firement station
      * @param _DescriptionEvent This is the description of the Event
@@ -45,28 +50,22 @@ contract Main is Ownable {
      * @param _Date this is the date of the event
      */
     function FillOutReport(
-            string memory _Name,
-            string memory _PhoneNumber,
-            string memory _DescriptionEvent, 
-            string memory _Location,
-            string memory _ReportTime,
-            string memory _AttendedCall,
-            string memory _DisplacementFiremenTime,
-            string memory _Observations,
-            string memory _FiremenSign,
-            string memory _Date
-        ) 
-        public 
-        view
-        onlyFiremen
-        Status
-    {
-
+        string memory _Name,
+        string memory _PhoneNumber,
+        string memory _DescriptionEvent,
+        string memory _Location,
+        string memory _ReportTime,
+        string memory _AttendedCall,
+        string memory _DisplacementFiremenTime,
+        string memory _Observations,
+        string memory _FiremenSign,
+        string memory _Date
+    ) public view onlyFiremen Status {
         Types.SIDIType memory report;
         report.Name = _Name;
-        report.PhoneNumber = _PhoneNumber; 
-        report.DescriptionEvent = _DescriptionEvent; 
-        report.Location =_Location;
+        report.PhoneNumber = _PhoneNumber;
+        report.DescriptionEvent = _DescriptionEvent;
+        report.Location = _Location;
         report.ReportTime = _ReportTime;
         report.AttendedCall = _AttendedCall;
         report.DisplacementFiremenTime = _DisplacementFiremenTime;
@@ -76,25 +75,24 @@ contract Main is Ownable {
     }
 
     /// @dev Set the firemen rol
-    function SetFiremenRol() public onlyOwner {
-        isFireman[msg.sender] = true;
+    function SetFiremenRol(address _FiremenAddress) public onlyOwner {
+        isFireman[_FiremenAddress] = true;
     }
 
     /// @dev Revoke the firemen rol
-    function RevokeFiremenRol() public onlyOwner{
-        isFireman[msg.sender] = false;
+    function RevokeFiremenRol(address _FiremenAddress) public onlyOwner {
+        isFireman[_FiremenAddress] = false;
     }
 
     /// @dev Pause the contract calls
-    function PuaseContract() public onlyOwner{
+    function PuaseContract() public onlyOwner returns (bool) {
         StatusPaused = true;
+        return StatusPaused;
     }
 
     /// @dev Unpause the contract calls
-    function UnpauseContract() public onlyOwner{
+    function UnpauseContract() public onlyOwner returns (bool) {
         StatusPaused = false;
+        return StatusPaused;
     }
-
-
-
 }
