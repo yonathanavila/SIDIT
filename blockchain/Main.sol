033cc8e90a4da4eaa1d6fde108ea91734a1bd12d
Yonathan Cruz <yonathancruz2019@gmail.com>
2022-11-26 16:32:06 -0600

    Fix the contract roles, add rol to edit

--- blockchain/Main.sol
+++ blockchain/Main.sol
@@ -8,17 +8,35 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Main is Ownable {
     bool public StatusPaused;
 
+    /// @dev event after register report
+    event FilledReport(address FiremenAddress, string Name, string HashId);
+
+    /// @dev event to edit existing report
+    event EditFilledReport(address FiremenAddress, string Name, string HashId);
+
     /// @dev Mapping to identify the sender role
     mapping(address => bool) isFireman;
 
+    /// @dev Mapping to identify the Editing report role
+    mapping(address => bool) canEditReport;
+
     /// @dev Mapping to indentify the report
-    mapping(uint256 => Types.SIDIType) IdentifyReport;
+    mapping(string => Types.SIDIType) IdentifyReport;
 
     /// @dev Modifier to identify the status pausable of the contract
     modifier Status() {
         require(
-            StatusPaused == true,
+            !StatusPaused,
             "The contract is paused you can't call this function"
+        );
+        _;
+    }
+
+    /// @dev Modifier to identify the status pausable of the contract
+    modifier onlyEditingRol() {
+        require(
+            canEditReport[msg.sender],
+            "You don't have access to this function"
         );
         _;
     }
@@ -26,7 +44,7 @@ contract Main is Ownable {
     /// @dev Modifier to validate the user rol
     modifier onlyFiremen() {
         require(
-            isFireman[msg.sender] == false,
+            isFireman[msg.sender],
             "You don't have access to this function"
         );
         _;
@@ -50,6 +68,7 @@ contract Main is Ownable {
      * @param _Date this is the date of the event
      */
     function FillOutReport(
+        string memory _HashId,
         string memory _Name,
         string memory _PhoneNumber,
         string memory _DescriptionEvent,
@@ -60,7 +79,7 @@ contract Main is Ownable {
         string memory _Observations,
         string memory _FiremenSign,
         string memory _Date
-    ) public view onlyFiremen Status {
+    ) public onlyFiremen Status {
         Types.SIDIType memory report;
         report.Name = _Name;
         report.PhoneNumber = _PhoneNumber;
@@ -72,16 +91,96 @@ contract Main is Ownable {
         report.Observations = _Observations;
         report.FiremenSign = _FiremenSign;
         report.Date = _Date;
+
+        IdentifyReport[_HashId] = report;
+
+        emit FilledReport(msg.sender, IdentifyReport[_HashId].Name, _HashId);
     }
 
-    /// @dev Set the firemen rol
+    /// @dev Edit report by Firemen Admin
+    function EditReport(
+        string memory _HashId,
+        string memory _Name,
+        string memory _PhoneNumber,
+        string memory _DescriptionEvent,
+        string memory _Location,
+        string memory _ReportTime,
+        string memory _AttendedCall,
+        string memory _DisplacementFiremenTime,
+        string memory _Observations,
+        string memory _FiremenSign,
+        string memory _Date
+    ) public onlyEditingRol Status {
+        Types.SIDIType memory report;
+        report.Name = _Name;
+        report.PhoneNumber = _PhoneNumber;
+        report.DescriptionEvent = _DescriptionEvent;
+        report.Location = _Location;
+        report.ReportTime = _ReportTime;
+        report.AttendedCall = _AttendedCall;
+        report.DisplacementFiremenTime = _DisplacementFiremenTime;
+        report.Observations = _Observations;
+        report.FiremenSign = _FiremenSign;
+        report.Date = _Date;
+
+        IdentifyReport[_HashId] = report;
+
+        emit EditFilledReport(
+            msg.sender,
+            IdentifyReport[_HashId].Name,
+            _HashId
+        );
+    }
+
+    /**
+     * @dev Search Report by report hash id
+     * @param _HashId This is the hash if of the report to consult
+     **/
+    function GetRepor(string memory _HashId)
+        public
+        view
+        returns (Types.SIDIType memory)
+    {
+        return IdentifyReport[_HashId];
+    }
+
+    /**
+     * @dev Set the firemen rol
+     * @param _FiremenAddress This is  the addres to set as Firemen rol
+     **/
     function SetFiremenRol(address _FiremenAddress) public onlyOwner {
         isFireman[_FiremenAddress] = true;
     }
 
-    /// @dev Revoke the firemen rol
+    /**
+     * @dev Revoke the firemen rol
+     * @param _FiremenAddress This function revoke the Firemen rol
+     **/
     function RevokeFiremenRol(address _FiremenAddress) public onlyOwner {
         isFireman[_FiremenAddress] = false;
+    }
+
+    /**
+     * @dev Set the Editing rol
+     * @param _FiremenAddress This is  the addres to Edit a report
+     **/
+    function SetEditingRol(address _FiremenAddress) public onlyOwner {
+        canEditReport[_FiremenAddress] = true;
+    }
+
+    /**
+     * @dev Revoke the Editing rol
+     * @param _FiremenAddress This function revoke the Editing rol
+     **/
+    function RevokeEditingRol(address _FiremenAddress) public onlyOwner {
+        canEditReport[_FiremenAddress] = false;
+    }
+
+    /**
+     * @dev Verify the rol of the sender
+     **/
+    function IsFiremen() public view returns (bool) {
+        return isFireman[msg.sender];
     }
 
     /// @dev Pause the contract calls
