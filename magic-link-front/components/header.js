4edcb7caa755bd30d885a7bf282fa4049fe91f0e
Yonathan Cruz <yonathancruz2019@gmail.com>
2022-11-27 12:47:54 -0600

    Login con magic eden

--- magic-link-front/components/header.js
+++ magic-link-front/components/header.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import Router from 'next/router';
 import { magic } from '../lib/magic';
@@ -16,67 +17,55 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <nav>
-        <ul>
+    <>
+      <nav className="flex h-9 items-center justify-between" aria-label="Global">
+        <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <Image className="h-8" height={50} width={50} src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white-700">
+            <span className="sr-only">Open main menu</span>
+            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
           {user?.loading ? (
             // If loading, don't display any buttons specific to the loggedIn state
             <div style={{ height: '38px' }}></div>
           ) : user?.issuer ? (
             <>
-              <li>
-                <Link href='/'>
-                  <TextButton color='primary' size='sm'>
-                    Home
-                  </TextButton>
-                </Link>
-              </li>
-              <li>
-                <Link href='/profile'>
-                  <TextButton color='primary' size='sm'>
-                    Profile
-                  </TextButton>
-                </Link>
-              </li>
-              <li>
-                <a>
-                  <TextButton color='warning' size='sm' onPress={logout}>
-                    Logout
-                  </TextButton>
-                </a>
-              </li>
+              <Link href='/'>
+                <a className="font-semibold text-white-900 hover:text-white-900">Home</a>
+              </Link>
+              <Link href='/profile'>
+                <a className="font-semibold text-white-900 hover:text-white-900">Profile</a>
+              </Link>
+              <Link href='/about'>
+                <a href="#" className="font-semibold text-white-900 hover:text-white-900">About</a>
+              </Link>
+              <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+                <TextButton color='warning' size='sm' className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-white-900 shadow-sm ring-1 ring-white-900/10 hover:ring-white-900/20" onPress={logout}>
+                  Logout
+                </TextButton>
+              </div>
             </>
           ) : (
-            <li>
-              <Link href='/login'>
-                <CallToAction color='primary' size='sm'>
+            <Link href='/login'>
+              <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+                <CallToAction className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-white-900 shadow-sm ring-1 ring-white-900/10 hover:ring-white-900/20">
                   Login
                 </CallToAction>
-              </Link>
-            </li>
+              </div>
+            </Link>
           )}
-        </ul>
+        </div>
       </nav>
-      <style jsx>{`
-        nav {
-          max-width: 45rem;
-          margin: 0 auto 50px;
-          padding: 1.25rem 1.25rem;
-          border-bottom: 1px solid #f0f0f0;
-        }
-        ul {
-          display: flex;
-          list-style: none;
-        }
-        li {
-          margin-right: 1.5rem;
-          line-height: 38px;
-        }
-        li:first-child {
-          margin-left: auto;
-        }
-      `}</style>
-    </header>
+    </>
   );
 };
 
--- magic-link-front/pages/index.js
+++ magic-link-front/pages/index.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import Image from 'next/image';
+import Header from '../components/header';
 import { UserContext } from '../lib/UserContext';
 import Loading from '../components/loading';
 
@@ -11,7 +12,7 @@ const Home = () => {
       {
         user?.loading ? <Loading /> : user?.issuer &&
           <div>
-            <div className="isolate bg-dark">
+            <div className="isolate bg-white">
               <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
                 <svg className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fillOpacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
@@ -25,33 +26,7 @@ const Home = () => {
               </div>
               <div className="px-6 pt-6 lg:px-8">
                 <div>
-                  <nav className="flex h-9 items-center justify-between" aria-label="Global">
-                    <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
-                      <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <Image className="h-8" height={500} width={500} src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-                      </a>
-                    </div>
-                    <div className="flex lg:hidden">
-                      <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white-700">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                      </button>
-                    </div>
-                    <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
-                      <a href="#" className="font-semibold text-white-900 hover:text-white-900">Product</a>
-
-                      <a href="#" className="font-semibold text-white-900 hover:text-white-900">Features</a>
-
-                      <a href="#" className="font-semibold text-white-900 hover:text-white-900">Marketplace</a>
-
-                      <a href="#" className="font-semibold text-white-900 hover:text-white-900">Company</a>
-                    </div>
-                    <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
-                    </div>
-                  </nav>
+                  <Header />
                   <div role="dialog" aria-modal="true">
                     <div onFocus={() => true} className="fixed inset-0 z-10 overflow-y-auto bg-dark px-6 py-6 lg:hidden">
                       <div className="flex h-9 items-center justify-between">
@@ -105,13 +80,13 @@ const Home = () => {
                         <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">SIDIT PROTOCOL</h1>
                         <p className="mt-6 text-lg leading-8 text-white-600 sm:text-center">A decentralized solution for the digitalization of the Honduras Fire Fighter reporting system that encourages transparency and efficiency.</p>
                         <div className="mt-8 flex gap-x-4 sm:justify-center">
-                          <a href="#" className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-dark shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
+                          <a href="#" className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-white-600 hover:bg-white-700 hover:ring-white-700">
                             Get started
-                            <span className="text-indigo-200" aria-hidden="true">&rarr;</span>
+                            <span className="text-white-200" aria-hidden="true">&rarr;</span>
                           </a>
-                          <a href="#" className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-dark-900 ring-1 ring-dark-900/10 hover:ring-dark-900/20">
+                          <a href="#" className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-white-900 ring-1 ring-white-900/10 hover:ring-white-900/20">
                             Live demo
-                            <span className="text-dark-400" aria-hidden="true">&rarr;</span>
+                            <span className="text-white-400" aria-hidden="true">&rarr;</span>
                           </a>
                         </div>
                       </div>
@@ -133,7 +108,6 @@ const Home = () => {
             </div>
           </div>
       }
-
     </>
   );
 };
